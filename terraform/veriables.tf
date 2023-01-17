176758664a75c45ce9b79572ae664d2c6cbad5e3
Mkatibashvili <62959761+Mkatibashvili@users.noreply.github.com>
2023-01-18 02:01:37 +0400

    Add files via upload
    finish files

---
+++ terraform/veriables.tf
@@ -0,0 +1,24 @@
+variable vpc_cidr_block {
+    default = "10.0.0.0/16"
+}
+variable subnet_cidr_block {
+    default = "10.0.10.0/24"
+}
+variable avail_zone {
+    default = "eu-central-1"
+}
+variable env_prefix {
+    default = "dev"
+}
+variable my_ip {
+    default = "212.58.120.33/32"
+}
+variable jenkins_ip {
+    default = "165.156.140.188/32"
+}
+variable instance_type {
+    default = "t2.micro"
+}
+variable region {
+    default = "eu-central-1"
+}
